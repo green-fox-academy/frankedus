ffcbb7b84bfb7d6d3d8a7647b1a497e08607047f
Edina Frank <frankedus@gmail.com>
2016-01-26 16:26:40 +0100

    exam exercises

---
+++ js_practice/exam_cars.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// Create a constructor called CarStore, that takes an array of cars as a parameter:
+// It should have an "addCar" method that adds a car object to it's list
+// It should have a "getSumPrice" method that returns the sum of all cars price
+// It should have a "getOldestCarType" method that returns the oldest car's type
+// It should have a "deleteCarByType" method that removes the cars from the inner list
+// that have the given type
+
+function CarStore (cars) {
+  this.cars = cars;
+
+  this.addCar = function (type, price, year) {
+    this.cars.push({type: type, price: price, year: year})
+  }
+
+  this.getSumPrice = function () {
+    var carsPrice = 0;
+    this.cars.forEach(function(car) {
+      carsPrice += car.price
+    })
+    return carsPrice;
+  }
+
+  this.getOldestCarType = function () {
+    var oldestCar = this.cars[0];
+    for (var i = 1; i < this.cars.length; i++) {
+      if (this.cars[i].year < oldestCar.year) {
+        oldestCar = this.cars[i];
+      }
+    }
+    return oldestCar.type;
+  }
+
+  this.deleteCarByType = function (type) {
+    var newArrayOfCars = [];
+    for (var i = 0; i < this.cars.length; i++) {
+      if (this.cars[i].type !== type) {
+        newArrayOfCars.push(this.cars[i]);
+      }
+    }
+    this.cars = newArrayOfCars
+  }
+
+}
+
+var cars = [
+  {type: 'Dodge', price: 20000, year: 2012},
+  {type: 'Tesla', price: 30000, year: 2015},
+  {type: 'Ferrari', price: 45000, year: 2013},
+  {type: 'Nissan', price: 12000, year: 2010},
+  {type: 'Trabant', price: 2000, year: 1980},
+  {type: 'Ferrari', price: 40000, year: 2001},
+  {type: 'Ferrari', price: 80000, year: 2010}
+];
+
+var carStore = new CarStore(cars);
+
+carStore.addCar('Smart', 18000, 2011);
+console.log(carStore.getSumPrice()); // 122000
+console.log(carStore.getOldestCarType()); // 'Trabant'
+carStore.deleteCarByType('Ferrari');
+console.log(carStore.getSumPrice()); // 82000
---
+++ js_practice/exam_circle.js
@@ -0,0 +1,21 @@
+'use strict';
+function Circle(radius) {
+  this.radius = radius;
+  this.getCircumference = function () {
+    return 2 * Math.PI * this.radius;
+    }
+  this.getArea = function () {
+    return Math.pow(this.radius, 2) * Math.PI
+    }
+  this.toString = function () {
+    return 'Radius: ' + this.radius + ', Circumference: ' + this.getCircumference() + ' Area: ' + this.getArea();
+    }
+}
+var circle = new Circle(4);
+console.log(circle.toString());
+// Create a Circle constructor that creates a circle object:
+// it should take the circle's radius as a parameter
+// it should have a "getCircumference" method that returns it's circumference
+// it should have a "getArea" method that returns it's area
+// it should have a "toString" method that returns it's stats as a string like:
+// 'Radius: 4, Circumference: 25.132741228718345, Area: 50.26548245743669'
